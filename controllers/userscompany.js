8cc530bfe7d6b03bf9dc413031e32c0f05918c84
mgmdi <14-10444@usb.ve>
2019-07-24 14:05:23 -0400

    Controlador para agregar a la relacion el usuario esta asociado a x compania

---
+++ controllers/userscompany.js
@@ -0,0 +1,58 @@
+const UsersCompany = require('../models').usersCompanies;
+
+
+/* Esta funcion busca si ya existe el usuario con la misma compania, sino entonces 
+agrega una nueva tupla que consiste en (userID, nuevo id de compania) */
+
+const searchUserID = (userID, companyID) => {
+    return (UsersCompany.findAll({
+        where: {
+            userID: userID,
+            companyID: companyID
+        }
+    })).then(function (user) {
+
+        if (user.length > 0)
+            // aqui va un next que maneja lo que se hara si se consigue un user / email iguales
+            return true;
+
+        else return false;
+        
+
+    })
+}
+
+/*Esta funcion crea la nueva tupla y la inserta en la bd */
+const createNewUserCompany = async (userID, companyID, res) => {
+    try {
+        console.log("Estoy en crear uusario")
+        const user = await UsersCompany.create({
+            userID,
+            companyID
+        })
+        console.log("hola aqui el usuario")
+        console.log(user)
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+/*Esta funcion es el controlador de llamadas para hacer el proceso de agregar un nuevo usersCompany */
+exports.createUsersCompany = async (userID, companyID, res) => {
+    try {
+        searchUserID(userID, companyID).then((existCompany)=>{
+            if(existCompany){
+                res.json('Ya la comania esta asociada al usuario')
+            }else{
+                createNewUserCompany(userID, companyID,res)
+            }
+
+        })
+
+        
+
+    } catch (err) {
+
+        return console.log("Error")
+    }
+}
