da7dbdb5851b3c4e86ac14758f5e58541bd19dc5
mgmdi <14-10444@usb.ve>
2019-07-24 14:03:18 -0400

    - Controlador que maneja /register. Crea un usuario en la bd. 	- FALTA: probar casos de crear usuario

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,11 @@
 const { check, validationResult } = require('express-validator');
 const db = require('../models/index.js')
 const User = require('../models').Users;
+const usersCompanyCont = require('./userscompany')
 
+/*
+DESCRIPTION: validates email and password (does not check if email exists)
+ */
 exports.validate = (method) => {
     switch (method) {
         case 'createUser': {
@@ -14,37 +18,82 @@ exports.validate = (method) => {
     }
 }
 
-const isUnique = (email,username) => {
-    User.findAll({
-        where: {
-            email: email,
-            username: username
-        }
-    }).done(function(error, user) {
+/****************************** USER SIGNUP ****************************/
 
-        if (error)
-            return console.log(error);
 
-        if (user)
-            // We found a user with this email address.
-            // Pass the error to the next method.
-            return res.json("El email o user no son unicos");
-        next();
+/* Esta funcion chequa los formatos de email y password y controla las llamadas para el
+proceso de agregar un usuario */
 
-    });
-}
+exports.createUser = (req, res) => {
 
-exports.createUser = async (req, res, next) => {
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             res.status(422).json({ errors: errors.array() });
             return;
         }
-        const { email, username, password } = req.body
+        const { email, username, password, companyID } = req.body
 
-        isUnique(email,username)
+        checkIfExist(email).then((result) => {
+            console.log('ESTE ES EXIST DIOS MIO')
+            const exist = result.exist
+            const user = result.user
+            console.log(user.dataValues)
+            if (exist) {
+                (user.dataValues.password === password ? usersCompanyCont.createUsersCompany(user[0].id, companyID, res) :
+                    res.json('El password no coincide'))
+            } else {
+                createNewUser(email, username, password, companyID, res)
+            }
+        })
 
+
+    } catch (err) {
+        return console.log(err)
+    }
+}
+
+
+
+/*
+DESCRIPTION: finds if email exists, if exists then calls next ==> next checks if email and password match in db
+if does not exist then calls createNewUser and continues 
+ */
+/*
+CASO BASE: el email es unico (no existe en la bd) => Agrego ***************LISTO**************
+                                                            1) El usuario en la base de datos
+                                                            2) El usuario + comania en la relacion usersCompanies
+CASO 1: el email existe y coincide con la contrasena en la BD => 
+                                                            1) Agrego en la tabla usersCompany que ya maneja que el usuario ya
+                                                            tenga esa compania. Si no esta enviar response de que ya posee la compania
+CASO 2: el email existe pero la contrasena no coincide con la bd => 
+                                                            1) Enviar response que diga eso
+
+ */
+
+/* Esta funcion chequea si existe el email, sino continua con agregar un nuevo usuario */
+const checkIfExist = (email) => {
+    return (User.findAll({
+        where: {
+            email: email,
+        }
+    })).then(function (user) {
+
+        if (user.length > 0) // Aqui debo manejar que si el email y contrasena coinciden, entonces le agrego el companyID
+            return { exist: true, user: user[0] }
+        else return { exist: false, user: null }
+        // Falta probar agregando una nueva empresa
+        // Debo agregarle el response a la empresa
+
+
+    })
+}
+
+
+
+/* Esta funcion agrega el nuevo usuario en la bd */
+const createNewUser = async (email, username, password, companyID, response) => {
+    try {
         const user = await User.create({
 
             email,
@@ -53,11 +102,15 @@ exports.createUser = async (req, res, next) => {
 
             password
         })
-        // Debo agregar a userscompany el id que traiga
-        res.json(user)
-    } catch (err) {
-        return next(err)
+
+        usersCompanyCont.createUsersCompany(user.id, companyID, response)
+
+        response.json(user)
+    } catch (e) {
+        console.log(e)
+        console.log('Error caught');
     }
 }
 
-// exports.loginUser = 
+/****************************** USER LOGIN ****************************/
+
