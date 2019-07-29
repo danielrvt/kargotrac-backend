63bab5b52c73985a0a26a414af579f2451b2fc68
mgmdi <14-10444@usb.ve>
2019-07-28 22:23:35 -0400

    Added cors

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const db = require("./models")
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 
 
+app.use(cors())
 app.use(bodyParser.json())
-
 app.use(require('./api/user'))
 
 
@@ -14,5 +15,5 @@ db.sequelize.sync().then(() => {
     console.log("ACK")
 })
 
-app.listen(3000, () => console.log("App listening on port 8080!"))
+app.listen(8080, () => console.log("App listening on port 8080!"))
 
