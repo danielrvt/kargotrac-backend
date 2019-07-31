de714e217360cb19e4da18df55534de3fdb974d9
mgmdi <14-10444@usb.ve>
2019-07-31 18:26:35 -0400

    Change db config

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "username": process.env['USERNAME'],
+  "password": process.env['PASSWORD'],
+  "database": process.env['DATABASE'],
+  "host": process.env['HOST'],
+  "dialect": process.env['DIALECT'],
+  "operatorsAliases": process.env['OPERATOR_ALIASES'],
+}
