a861298913a3fa3fa67f9a9bcb02146279ee4d5e
mgmdi <14-10444@usb.ve>
2019-08-01 15:28:16 -0400

    fixing dialect

--- models/index.js
+++ models/index.js
@@ -8,20 +8,12 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js');
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config, 
-    {
-      host: config.host,
-      dialect: 'mysql'
-    });
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, 
-    {
-      host: config.host,
-      dialect: 'mysql'
-    });
-}
+let sequelize = new Sequelize(config.database, config.username, config.password,
+  {
+    host: config.host,
+    dialect: 'mysql'
+  });
+
 
 fs
   .readdirSync(__dirname)
