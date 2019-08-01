e06065b7ac68bdf8abb5923117420667c9cffd28
mgmdi <14-10444@usb.ve>
2019-08-01 11:51:59 -0400

    Added dialect to db config

--- config/config.js
+++ config/config.js
@@ -3,6 +3,5 @@ module.exports = {
   "password": process.env['PASSWORD'],
   "database": process.env['DATABASE'],
   "host": process.env['HOST'],
-  "dialect": process.env['DIALECT'],
   "operatorsAliases": process.env['OPERATOR_ALIASES'],
 }
