b795dff9c202292d4e916979ca86e3f12b3394f0
mgmdi <14-10444@usb.ve>
2019-08-02 22:12:06 -0400

    added netlify origin

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ db.sequelize.sync().then(() => {
     console.log("ACK")
 })
 
-app.listen(process.env.PORT, () => console.log("App listening on port 8080!"))
+app.listen(process.env.PORT || 5000, () => console.log("App listening on port 8080!"))
 
