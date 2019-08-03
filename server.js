f38d424b13ceed08b6162ce996851a09cc979fbc
mgmdi <14-10444@usb.ve>
2019-08-02 21:22:05 -0400

    trying to fix cors issue

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 
-app.use(cors())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+//app.use(cors({credentials: true, origin: true}))
+const options = {
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Access-Control-Allow-Origin"],
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    origin: true,
+    preflightContinue: false
+};
+
+//use cors middleware
+app.use(cors(options));
 
 
 app.use(bodyParser.json())
