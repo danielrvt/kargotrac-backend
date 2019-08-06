d97152238e99df564834a3d2a7ea252b938170b5
mgmdi <14-10444@usb.ve>
2019-08-06 00:14:29 -0400

    deleted allownull: false ==> arrojo errores desconocidos

--- models/item.js
+++ models/item.js
@@ -1,19 +1,24 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Item = sequelize.define('Item', {
+  const Items = sequelize.define('Items', {
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+
     },
     quantity: {
       type: DataTypes.INTEGER,
-      allowNull: false
-    }
+
+    },
+    companyID: {
+      type: DataTypes.INTEGER,
+
+    }, 
   }, {});
-  Item.associate = function (models) {
-    Item.belongsTo(models.Package)
-    Item.belongsTo(models.Shipment)
+  Items.associate = function (models) {
+    Items.belongsTo(models.Packages, { as: 'Packages', foreignKey: 'id' })
+    Items.belongsTo(models.Shipments)
+    Items.belongsTo(models.Users, { as: 'User', foreignKey: 'id' })
     // associations can be defined here
   };
-  return Item;
+  return Items;
 };
