e4bf6d2e7a693dbb7b5eb47140c9cd952b325711
mgmdi <14-10444@usb.ve>
2019-08-06 00:11:58 -0400

    items controllers w create item func

---
+++ controllers/item.js
@@ -0,0 +1,85 @@
+const User = require('../models').Users;
+const Packages = require('../models').Packages;
+const Items = require('../models').Items;
+
+// ******************************* GESTION DE ARTICULOS ******************************
+
+const create = async (name, quantity, companyID) => {
+    let item = {};
+    try {
+        item = await Items.create(
+            name,
+            parseInt(quantity),
+            parseInt(companyID),
+
+        )
+    } catch (e) {
+        console.log(e)
+        console.log('Error caught');
+    }
+    return item
+}
+
+exports.createItem = (req, res) => {
+
+    const { name, quantity, tracking_id, companyID, userID } = req.body
+
+    // Si el tracking_id no existe then createPackage
+    // else just add the item
+
+    // Creo el paquete 
+
+    Packages.findOrCreate({
+        where: {
+            tracking_id: tracking_id
+        },
+        defaults: {
+            tracking_id: tracking_id
+        }
+    }).then((package) => {
+        // Creo el item
+
+        create(
+            name,
+            parseInt(quantity),
+            parseInt(companyID)
+        ).then((item) => {
+            // Seteo las relaciones
+
+            // Relacion articulos estan en paquetes
+            item.setPackages(package[0].dataValues.id)
+
+            User.findOne({
+                where: {
+                    id: userID
+                }
+            }).then((user) => {
+                
+                // Relacion el articulo esta asociado a un usuario
+                item.setUser(user.dataValues.id)
+
+                const response = {
+                    tracking_id: tracking_id,
+                    status: null,
+                    name: name,
+                    qty: quantity,
+                    item_id: item.dataValues.id,
+                    package_id: package[0].dataValues.id
+
+                }
+                console.log(response)
+                res.json(response)
+
+            })
+
+
+
+        })
+
+
+
+
+
+    })
+
+}
