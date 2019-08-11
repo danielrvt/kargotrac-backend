7d95ce322c61fffb03b466509da8d4cbabe10c42
mgmdi <14-10444@usb.ve>
2019-08-11 18:56:18 -0400

    i think now user can login without companyID

--- controllers/user.js
+++ controllers/user.js
@@ -202,7 +202,7 @@ exports.login = (req, res) => {
                                 })
                             } else {
                                 let token = jwt.sign({ id: user.dataValues.id, username: user.dataValues.username }, 'whatever it takes', { expiresIn: 129600 }); // Sigining the token
-                                res.json({ status: 'success', user: user.dataValues, companyID: companies[0], token: token, usersCompanies: companies })
+                                res.json({ status: 'success', user: user.dataValues, companyID: companies[0].dataValues.companyID, token: token, usersCompanies: companies })
                             }
 
                         } else {
