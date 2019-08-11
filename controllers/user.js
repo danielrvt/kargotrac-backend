7db940bf3bede36e19f3a27d9c2690e45ae47984
mgmdi <14-10444@usb.ve>
2019-08-11 18:23:27 -0400

    i think now user can login without companyID

--- controllers/user.js
+++ controllers/user.js
@@ -61,6 +61,8 @@ exports.createUser = (req, res) => {
                                         companies.map((company) => {
                                             return company.dataValues.companyID
                                         })
+                                        console.log("companiasss!!!!!!!!!!!!!!!!!!!!!")
+                                        console.log(companies)
                                         findUsersCompanies(user.dataValues.id).then((companies) => {
                                             let token = jwt.sign({ id: user.dataValues.id, username: user.dataValues.username }, 'whatever it takes', { expiresIn: 129600 }); // Sigining the token
                                             res.json({ status: 'success', user: user.dataValues, companyID: companyID, token: token, usersCompanies: companies })
