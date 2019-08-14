e56d7e41ef4c1ae43266baa0a23adf99f5158d74
Daniel Rodriguez del Villar Trimarchi <daniel@dnd1.com>
2019-08-14 12:18:51 -0400

    Fixed some config and added docker-compose

--- config/config.js
+++ config/config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  "username": "root",
-  "password": "mysql",
-  "database": "kargotrack_db",
-  "host": "localhost",
-  "dialect": 'mysql',
-  "operatorsAliases": false,
+    apiConfig: {
+        port: process.env.PORT || 8080,
+    },
+    dbConfig: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+        //  "operatorsAliases": false,}
+    },
 }
