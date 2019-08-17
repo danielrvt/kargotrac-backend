cd4a177fadcade79cc21670db3c0eb009b5c7228
mgmdi <14-10444@usb.ve>
2019-08-17 10:50:27 -0400

    merge with working branch

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "username": "root",
-  "password": "mysql",
-  "database": "kargotrack_db",
-  "host": "localhost",
-  "dialect": 'mysql',
-  "operatorsAliases": false,
+  "username": process.env['USERNAME'],
+  "password": process.env['PASSWORD'],
+  "database": process.env['DATABASE'],
+  "host": process.env['HOST'],
+  "operatorsAliases": process.env['OPERATOR_ALIASES'],
+
 }
